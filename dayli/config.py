4827969ea050d26c094f8fb81f6c9e23b98ae917
K44 <stas4964037@yandex.ru>
2024-02-02 15:20:00 +0300

    проблема подключения к бд, ошибка при реге юзера

--- dayli/config.py
+++ dayli/config.py
@@ -1,4 +1,4 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = \
-        'postgresql://postgres:123qwe@0.0.0.0:5432/dayli'
+        'postgresql://postgres:123qwe@pgbd:5432/dayli'
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
---
+++ dayli/users/routes.py
@@ -0,0 +1,29 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from dayli import db, bcrypt
+from dayli.models import User, Post
+from dayli.users.forms import (RegistrationForm, LoginForm,
+                               UpdateAccountForm, RequestResetForm,
+                               ResetPasswordForm)
+
+from dayli.users.utils import save_picture
+
+users = Blueprint('users', __name__)
+
+
+@users.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Ваша учетная запись была создана!'
+              ' Теперь вы можете войти в систему', 'success')
+        return redirect(url_for('users.login'))
+    return render_template('register.html', title='Register', form=form)
