42f9ce1471ffa78ae371116af69a409b1266dac0
K44 <stas4964037@yandex.ru>
2024-02-03 17:07:09 +0300

    posting by user added

--- dayli/posts/routes.py
+++ dayli/posts/routes.py
@@ -13,5 +13,20 @@ posts = Blueprint('posts', __name__)
 def allpost():
     page = request.args.get('page', 1, type=int)
     allposts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,
-                                                                  per_page=5)
+                                                                     per_page=5)
     return render_template('allpost.html', posts=allposts)
+
+
+@posts.route('/post/new', methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data,
+                    user_id=current_user.get_id())
+        db.session.add(post)
+        db.session.commit()
+        flash('Ваш пост создан!')
+        return redirect(url_for('posts.allpost'))
+    return render_template('create_post.html', title="Новый пост",
+                           form=form, legend="Новый пост")
