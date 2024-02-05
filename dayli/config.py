b616e5b33a3e61431b9474bbd3053378043272b2
K44 <stas4964037@yandex.ru>
2024-02-05 11:40:17 +0300

    gitignore fix

--- dayli/config.py
+++ dayli/config.py
@@ -7,5 +7,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "kashey1@gmail.com"
-    MAIL_PASSWORD = "eugm hpdt naac ymgp"
+    MAIL_USERNAME = ""
+    MAIL_PASSWORD = ""
